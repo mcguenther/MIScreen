194ea1d3303befdd91789ad88b70ddaf9f551a1c
Johannes Dorn <johannes@dorn.tk>
2016-06-01 16:47:40 +0200

    implemented first view which allows user to choose role of the device

--- MIScreen/app/src/main/java/de/volzo/miscreen/MainActivity.java
+++ MIScreen/app/src/main/java/de/volzo/miscreen/MainActivity.java
@@ -1,13 +1,68 @@
 package de.volzo.miscreen;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.Spinner;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener, View.OnClickListener {
+    private Spinner spRole;
+    private Button btOk;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        spRole = (Spinner) findViewById(R.id.spRole);
+        btOk = (Button) findViewById(R.id.btOk);
+
+        spRole.setOnItemSelectedListener(this);
+        btOk.setOnClickListener(this);
+    }
+
+
+    // as taken from https://developer.android.com/guide/topics/ui/controls/spinner.html
+    public void onItemSelected(AdapterView<?> parent, View view,
+                               int pos, long id) {
+        // An item was selected. You can retrieve the selected item using
+        // parent.getItemAtPosition(pos)
+        saveRole(pos);
+    }
+
+    private void saveRole(int pos) {
+        SharedPreferences sharedPref = getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putInt("role", pos);
+        editor.commit();
+    }
+
+    public void onNothingSelected(AdapterView<?> parent) {
+        resetView();
+    }
+
+    private void resetView() {
+        saveRole(0);
+        this.spRole.setSelection(0);
+    }
+
+    @Override
+    public void onClick(View v) {
+        // call next activity
+    }
+
+    private void refreshView() {
+        SharedPreferences sharedPref = getPreferences(MODE_PRIVATE);
+
+        int role = sharedPref.getInt("role", 0);
+        this.spRole.setSelection(role);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        refreshView();
     }
 }
