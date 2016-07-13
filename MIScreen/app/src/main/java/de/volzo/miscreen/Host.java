a927334a8471231228b33df515e2e8aebbbd88e5
Johannes Dorn <johannes@dorn.tk>
2016-07-13 15:41:50 +0200

    created method body in host class

--- MIScreen/app/src/main/java/de/volzo/miscreen/Host.java
+++ MIScreen/app/src/main/java/de/volzo/miscreen/Host.java
@@ -1,5 +1,9 @@
 package de.volzo.miscreen;
 
+import java.util.List;
+
+import de.volzo.miscreen.arbitraryBoundingBox.ArbitrarilyOrientedBoundingBox;
+
 /**
  * Created by Johannes on 01.06.2016.
  */
@@ -23,5 +27,19 @@ public class Host {
         if(mHost != null) {
             mHost = null;
         }
+    }
+
+    // input: transformation matrices
+    //TODO check if matrices have the correct format
+    private static ArbitrarilyOrientedBoundingBox getAOBB(float[] hostF, List<float[]> fList) {
+
+        // TODO: calc transformation matrices relative to fMatrix of host (hostF)
+        // TODO: calc 2D points from fMatrices
+        // TODO: call constructor of AOBB with 2D points
+
+
+
+
+        return null;
     }
 }
