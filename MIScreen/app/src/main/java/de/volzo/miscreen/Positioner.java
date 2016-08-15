f93c766ea85956768a5af325f987a19b5cd66e9f
Johannes Dorn <johannes@dorn.tk>
2016-08-15 18:39:26 +0200

    enabled fullscreen mode for positioner

--- MIScreen/app/src/main/java/de/volzo/miscreen/Positioner.java
+++ MIScreen/app/src/main/java/de/volzo/miscreen/Positioner.java
@@ -12,6 +12,8 @@ import android.support.v4.content.ContextCompat;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Display;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -49,6 +51,11 @@ public class Positioner extends ARActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_positioner);
 
 
